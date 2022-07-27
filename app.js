fddfc6a25e6711b351d2743c5ce00bd43564ea29
umairahmed88 <umairahmed_88@ymail.com>
2022-07-27 13:39:08 +0500

    Added error handling errors to the app

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controller/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
@@ -16,15 +18,16 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json());
 app.use(express.static(`${__dirname}/../public`));
 
-app.use((req, res, next) => {
-	console.log("Hello from the middleware!");
-	next();
-});
-
 // Routes
 
 app.use("/api/v1/tours", tourRouter);
 
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+	next(new AppError(`Can't find ${req.originalUrl} on this server!`));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- models/tourModels.js
+++ models/tourModels.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const validator = require("validator");
 
 const tourSchema = new mongoose.Schema(
 	{
@@ -8,6 +9,8 @@ const tourSchema = new mongoose.Schema(
 			required: [true, "A tour must have name"],
 			unique: true,
 			trim: true,
+			maxlength: [40, "A tour name must have less or equal then 40 characters"],
+			minlength: [10, "A tour name must have more or equal then 10 characters"],
 		},
 		slug: String,
 		duration: {
@@ -21,10 +24,16 @@ const tourSchema = new mongoose.Schema(
 		difficulty: {
 			type: String,
 			required: [true, "A tour must have a difficulty type."],
+			enum: {
+				values: ["easy", "medium", "difficult"],
+				message: "Difficulty is either: easy, medium, difficult",
+			},
 		},
 		ratingsAverage: {
 			type: Number,
 			default: 4.5,
+			min: [1, "Rating must be above 1.0"],
+			max: [5, "Rating must be below 5.0"],
 		},
 		ratingsQuantity: {
 			type: Number,
@@ -34,7 +43,15 @@ const tourSchema = new mongoose.Schema(
 			type: Number,
 			required: [true, "A tour must have price"],
 		},
-		priceDiscount: Number,
+		priceDiscount: {
+			type: Number,
+			validate: {
+				validator: function (val) {
+					return val < this.price; // this only points to current doc on New document creations
+				},
+				message: "Discount price ({VALUE}) should be less than regular price",
+			},
+		},
 		summary: {
 			type: String,
 			trim: true,
@@ -103,6 +120,12 @@ tourSchema.pre(/^find/, function (next) {
 tourSchema.post(/^find/, function (docs, next) {
 	console.log(`Query took ${Date.now() - this.start} milliseconds!`); //this shows how long it start to execute
 	console.log(docs);
+	next();
+});
+
+tourSchema.pre("aggregate", function (next) {
+	this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+	console.log(this.pipeline());
 	next();
 });
 
